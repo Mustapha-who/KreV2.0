d64f9e22c7f3fbb3dd239d3d22a9641b6c134ead
mustapha jerbi <jerbimustapha3@gmail.com>
2025-08-22 12:17:20 +0100

    Add client folder as normal directory

---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,23 @@
+
+import React, { useEffect, useState } from "react";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import Navbar from "@/components/Navbar";
+
+//
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+    <div className="h-full w-full">
+      <Navbar />
+      <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        
+        
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
