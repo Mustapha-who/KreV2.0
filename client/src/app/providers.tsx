02456dfdee9dd884017a0839ca3f74dfe2614a57
mustapha jerbi <jerbimustapha3@gmail.com>
2025-08-24 10:28:16 +0100

    Configure AWS Cognito auth and add sign-in/signup pages

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import StoreProvider from "@/state/redux";
+import { Authenticator } from "@aws-amplify/ui-react";
+import  Auth  from "./(auth)/authProvider";
+import { Provider } from 'react-redux';
+
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <StoreProvider>
-        {children}
+            <Authenticator.Provider>
+                <Auth>{children}</Auth>
+            </Authenticator.Provider>
+        {children} 
         </StoreProvider>
     );
 };
