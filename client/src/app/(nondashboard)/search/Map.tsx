ec9b1a57ac43f3f4da6b683ef66f77de47ec29d5
mustapha jerbi <jerbimustapha3@gmail.com>
2025-09-02 21:24:07 +0100

    Add Mapbox for house locations, Listings page, Properties & Applications frontend (tenant favorites, manager view), and some cleanup

--- client/src/app/(nondashboard)/search/Map.tsx
+++ client/src/app/(nondashboard)/search/Map.tsx
@@ -22,7 +22,7 @@ const Map = () => {
 
     const map = new mapboxgl.Map({
       container: mapContainerRef.current!,
-      style: "mapbox://styles/majesticglue/cm6u301pq008b01sl7yk1cnvb",
+      style: "mapbox://styles/mustapha06/cmf2d1nqx000601qsenfdckv7",
       center: filters.coordinates || [-74.5, 40],
       zoom: 9,
     });
